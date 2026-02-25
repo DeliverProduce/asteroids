576a8ccac15aaf9e1b827d6e9cb021f946a04a80
DeliverProduce <benpjohns@hotmail.com>
2026-02-25 01:38:38 +0000

    added player and changes to constants/main

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import pygame
 from constants import SCREEN_WIDTH, SCREEN_HEIGHT
 from logger import log_state
+from player import Player
 
 
 def main():
@@ -11,12 +12,17 @@ def main():
 
     print(f"Starting Asteroids with pygame version: {pygame.version.ver}")
 
+    pos_x = SCREEN_WIDTH / 2
+    pos_y = SCREEN_HEIGHT / 2
+    player = Player(pos_x, pos_y)
+
     while True:
         log_state()
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
         screen.fill("black")
+        player.draw(screen)
         pygame.display.flip()
 
         dt = game_clock.tick(60) / 1000
